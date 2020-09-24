f6c9a7876a7decf1e7eb243c65b8099d3467ebc0
Mak <makotorasahi@gmail.com>
2020-09-23 21:30:53 -0700

    Updates

---
+++ precache-manifest.f082dfb559aee9c2dced359c6b2bf066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e671f74cfc4fe1143f8663ada5bea0",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "3c541ffdfc14fe00644e",
+    "url": "/reactportfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3c541ffdfc14fe00644e",
+    "url": "/reactportfolio/static/js/2.e9622369.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio/static/js/2.e9622369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8a7902bb101bc10fe0",
+    "url": "/reactportfolio/static/js/main.cc639009.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  }
+]);
