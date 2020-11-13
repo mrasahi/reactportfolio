6e3ccd8a50b497cb423f2540feaada04ce17da22
Mak <makotorasahi@gmail.com>
2020-11-13 03:31:01 -0800

    img src changed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap';
-
+import makMajestic from '../../assets/img/mak_majestic.png'
+import './Home.css'
 
 const Home = () => {
     return (
@@ -11,6 +12,13 @@ const Home = () => {
                     <Col>
                         <h1>Makoto Asahi</h1>
                         <h6>Full Stack Web Developer</h6>
+                        <img className="header-img img-fluid" title={"the greatest"} src={makMajestic} alt="Mak majestic"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <p>no u</p>
+                    
                     </Col>
                 </Row>
             </Container>
